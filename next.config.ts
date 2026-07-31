9e30a81bf1deb1fe6666ab97b9a1e865db538070
luisuarez09 <noreply@email.com>
2026-07-30 23:53:19 -0400

    Allow tablet access to Next development server

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.1.240"],
   outputFileTracingRoot: path.join(process.cwd()),
 };
 
